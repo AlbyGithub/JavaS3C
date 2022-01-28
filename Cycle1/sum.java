abca451c45f3f64b187edb20d7d2c804703db21d
AlbyGithub <98335471+AlbyGithub@users.noreply.github.com>
2022-01-28 21:22:11 +0530

    Update sum.java

--- Cycle1/sum.java
+++ Cycle1/sum.java
@@ -1,11 +1,15 @@
-class Sum
+import java.util.*;
+class Sum 
 {
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int a,b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println("Sum = "+ (a+b));
-	}
+    public static void main(String args[])
+    {
+     Scanner obj=new Scanner(System.in);
+     int a,b,sum;
+     System.out.println("Enter first number");
+     a=obj.nextInt();
+     System.out.println("Enter second number");
+     b=obj.nextInt();
+     sum=a+b;
+     System.out.println("Sum = "+sum);
+    }
 }
