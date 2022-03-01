c120efeadf35e61e083485cc8d2589d256540a51
AlbyGithub <98335471+AlbyGithub@users.noreply.github.com>
2022-03-01 16:30:31 +0530

    Update Exp1.java

--- Cycle7/Exp1.java
+++ Cycle7/Exp1.java
@@ -2,71 +2,45 @@ import java.util.*;
 class Square extends Thread
 
 {
-	
 	int n;
-	
 	Square(int n)
 	
 	{
-		
-		this.n = n;
-		
+		this.n = n;	
 	}
 	
 	public void run()
-	
 	{
-		
-		int s = this.n * this.n;
-		
+		int s = this.n * this.n;	
 		System.out.println("Square of " + this.n + " = " + s );
-		
 	}
-	
 }
-
 class Cube extends Thread
-
 {
-	
-	int n;
-	
+	int n;	
 	Cube(int n)
-	
 	{
 		this.n = n;
 	}
-	
 	public void run()
-	
 	{
-		
-		int c = this.n * this.n * this.n;
-		
+		int c = this.n * this.n * this.n;	
 		System.out.println("Cube of " + this.n + " = " + c );
-		
 	}
-	
 }
-
 class Number extends Thread
-
 {
-	
-	public void run()
-	
+	public void run()	
 	{
-		Scanner sc = new Scanner(System.in);
+	        Scanner sc = new Scanner(System.in);
 		System.out.print("How Many Random Integers needed : ");
 		int x = sc.nextInt();
 		Random random = new Random();
 		System.out.print("Set the limit upto which random numbers should be generated : ");
 		int l = sc.nextInt();
 		
-		for(int i =0; i<x; i++)
-			
+		for(int i =0; i<x; i++)	
 		{
-			
 			int ranInt = random.nextInt(l);
 			
 			System.out.println("The random integer: " + ranInt);
@@ -89,23 +63,16 @@ class Number extends Thread
 			catch (InterruptedException e) 
 			{
 				System.out.println(e);
-			}
-			
-		}
-		
-	}
-	
+			}	
+		}	
+	}	
 }
-
 public class Exp1 {
 	
 	public static void main(String args[]) throws Exception
-	
 	{
-		
 		Number n = new Number();
 		
-		n.start();
-		
+		n.start();	
 	}
 }
